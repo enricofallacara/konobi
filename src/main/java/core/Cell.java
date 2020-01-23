ce6302535b1fd368f508d4296729f59aa4fee0a8
pindri <patrick.indri@outlook.it>
2020-01-23 14:56:55 +0100

    Coordinate test

--- src/main/java/core/Cell.java
+++ src/main/java/core/Cell.java
@@ -1,12 +1,20 @@
 package core;
 
+import java.awt.*;
+
 public class Cell {
-    public Cell() {
+
+    public Cell(Point p) {
         color = cellStatus.empty;
-    };
+    }
     private cellStatus color;
+    private Point coordinates;
 
     public cellStatus getColor() {
         return color;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
     }
 }
--- src/test/java/core/cellTest.java
+++ src/test/java/core/cellTest.java
@@ -1,13 +1,17 @@
 package core;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.*;
+import java.awt.Point;
 
 
 public class cellTest {
     @Test
     public void testVariables() {
-       Cell cell = new Cell();
+       Cell cell = new Cell(new Point(3, 4));
        assertEquals(cell.getColor(), cellStatus.empty);
+       assertEquals(cell.getCoordinates(), new Point(3, 4));
     }
 }
