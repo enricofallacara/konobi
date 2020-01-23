d2682b57e65d5b286e0ce4e0a1b13fa101b5bf8c
enricofallacara <enrico.fallacara@gmail.com>
2020-01-23 15:48:28 +0100

    Cell coordinates test

--- src/main/java/core/Board.java
+++ src/main/java/core/Board.java
@@ -6,6 +6,11 @@ public class Board {
     public Board(int s){
         size = s;  // necessary to add a control on value of s > 0
         grid = new Cell[s][s];
+        for(int i = 0; i < s ; i++){
+            for(int j = 0; j < s; j++){
+                grid[i][j] = new Cell(new Point(i,j));
+            }
+        }
     }
     private int size;
     private Cell[][] grid;
