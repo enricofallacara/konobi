bb0106fad30167871317d4ac4754ec526e8e3d1f
enricofallacara <enrico.fallacara@gmail.com>
2020-01-23 15:43:08 +0100

    Cell coordinates test

--- src/main/java/core/Board.java
+++ src/main/java/core/Board.java
@@ -1,11 +1,16 @@
 package core;
 
+import java.awt.*;
+
 public class Board {
     public Board(int s){
-        size = s;
+        size = s;  // necessary to add a control on value of s > 0
+        grid = new Cell[s][s];
     }
     private int size;
+    private Cell[][] grid;
 
     public int getSize(){ return size;}
+    public Cell getCell(Point p){ return grid[p.x][p.y];}
 
 }
--- src/test/java/core/boardTest.java
+++ src/test/java/core/boardTest.java
@@ -1,6 +1,8 @@
 package core;
 import org.junit.Test;
 
+import java.awt.*;
+
 import static org.junit.Assert.*;
 public class boardTest {
 
@@ -8,5 +10,11 @@ public class boardTest {
     public void sizeTest(){
         Board board = new Board(11);
         assertEquals(board.getSize(), 11);
+    }
+
+    @Test
+    public void cellCoordinatesTest(){
+        Board board = new Board(11);
+        assertEquals(board.getCell(new Point(3,4)).getCoordinates(), new Point(3,4));
     }
 }
