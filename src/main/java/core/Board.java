7b5eda8c14a0fb6ab22140c7b9047d1cd4deaf5b
federicopigozzi <pigozzife@gmail.com>
2020-01-24 12:46:57 +0100

    improved grid filling with streams

--- src/main/java/core/Board.java
+++ src/main/java/core/Board.java
@@ -1,16 +1,19 @@
 package core;
 
-import java.awt.*;
+import java.awt.Point;
+import java.util.stream.IntStream;
 
 public class Board {
     public Board(int s){
         size = s;  // necessary to add a control on value of s > 0
-        grid = new Cell[s][s];
-        for(int i = 0; i < s ; i++){
-            for(int j = 0; j < s; j++){
-                grid[i][j] = new Cell(new Point(i,j));
-            }
-        }
+        //grid = new Cell[s][s];
+        grid = IntStream.range(0, s).mapToObj(x -> IntStream.range(0, s)
+                .mapToObj(y -> new Cell(new Point(x, y))).toArray(Cell[]::new)).toArray(Cell[][]::new);
+        //for(int i = 0; i < s ; i++){
+        //    for(int j = 0; j < s; j++){
+        //        grid[i][j] = new Cell(new Point(i, j));
+        //    }
+        //}
     }
     private int size;
     private Cell[][] grid;
