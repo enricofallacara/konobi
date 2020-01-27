de12b8034a9ffda17ef11f3819608a825fd2e0b4
pindri <patrick.indri@outlook.it>
2020-01-27 15:35:16 +0100

    Removed pre-refactoring commented code

--- src/main/java/core/ValidPositionRule.java
+++ src/main/java/core/ValidPositionRule.java
@@ -6,83 +6,10 @@ import java.util.ArrayList;
 
 public class ValidPositionRule {
     public static boolean query(ArrayList<PositionRule> rules, Point point, Board board, Player player) {
-        /*
-
-        ArrayList<Boolean> result = new ArrayList<>();
-
-        rules.stream().forEach(x -> x.isValid(point, board, player));
-
-        for(PositionRule rule : rules) {
-            result.add(rule.isValid(point, board, player));
-        }
-
-        return result.stream().allMatch(x -> x);
-
-         */
 
         return rules.stream().allMatch(x -> x.isValid(point, board, player));
 
     }
-
 }
 
 
-
-
-
-/*
-
-public class ValidPositionRule {
-    public static boolean query(Point point, Board board, Player player){
-        // TODO: PROBLEMA: SE QUALCUNO VUOLE AGGIUNGERE UNA REGOLA, DEVE AGGIUNGERE UN ELSE IF,
-        // CHE RENDE IL CODICE PIU RIGIDO E VIOLA L'OPEN CLOSED PRINCIPLE.
-        // TODO: LA SOLUZIONE E LA CLASSE RULEBOOK
-        if (hasCrosscut(point, board, player)) {
-            return false;
-        }
-        else if (!board.getColoredNeighbours(point, 1, player, Board::isStrongNeighbour).isEmpty()) {
-            return true;
-        }
-        else if (specialWeakPosition(point, board, player)) {
-            return true;
-        }
-        else if (!board.getColoredNeighbours(point, 1, player, Board::isWeakNeighbour).isEmpty()) {
-            return false;
-        }
-        return true;
-    }
-
-
-    public static boolean hasCrosscut(Point point, Board board, Player player) {
-        if(board.getColoredNeighbours(point, 1, player, Board::isWeakNeighbour).isEmpty()) {
-            return false;
-        }
-
-        ArrayList<Cell> neighbours = board.getColoredNeighbours(point, 1, player, Board::isWeakNeighbour);
-        for(Cell c : neighbours) {
-            if(board.getCell(new Point(point.x, c.getCoordinates().y)).getColor() == player.getOppositeColor()
-                    && board.getCell(new Point(c.getCoordinates().x, point.y)).getColor() == player.getOppositeColor()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static boolean specialWeakPosition(Point point, Board board, Player player) {
-        ArrayList<Cell> weakNeighbours = board.getColoredNeighbours(point, 1, player, Board::isWeakNeighbour);
-        ArrayList<Boolean> result = new ArrayList<>();
-
-        for(Cell c1 : weakNeighbours) {
-            ArrayList<Cell> strongOfWeak = board.getNeighbours(c1.getCoordinates(), 1, Board::isStrongNeighbour);
-            for(Cell c2 : strongOfWeak) {
-                if( c2.getColor() == null ) {
-                    result.add(!board.getColoredNeighbours(c2.getCoordinates(), 1, player, Board::isWeakNeighbour).isEmpty());
-                }
-            }
-        }
-        return result.stream().allMatch(x -> x == true);
-    }
-
-}
-
- */
--- src/test/java/core/validPositionRuleTest.java
+++ src/test/java/core/validPositionRuleTest.java
@@ -33,36 +33,6 @@ public class validPositionRuleTest {
         assertFalse(board.getColoredNeighbours(point, 1, player, Board::isWeakNeighbour).isEmpty());
     }
 
-/*
-    @Test
-    public void crosscutTest() {
-        Board board = new Board(11);
-        Player player = new Player(Color.black);
-        Point point = new Point(2, 3);
-        board.setCell(new Point(1, 2), Color.black);
-        board.setCell(new Point(2, 2), Color.white);
-        board.setCell(new Point(1, 3), Color.white);
-        assertTrue(ValidPositionRule.hasCrosscut(point, board, player));
-
-    }
-
-    @Test
-    public void specialWeakPositionTest() {
-        Board board = new Board(11);
-        Player player = new Player(Color.white);
-        Point point = new Point(1, 2);
-
-        board.setCell(new Point(0, 0), Color.white);
-        board.setCell(new Point(2, 1), Color.white);
-        board.setCell(new Point(3, 3), Color.white);
-
-        board.setCell(new Point(2, 0), Color.black);
-        board.setCell(new Point(3, 0), Color.black);
-        board.setCell(new Point(3, 1), Color.black);
-        assertTrue(ValidPositionRule.specialWeakPosition(point, board, player));
-    }
-
- */
 
     @Test
     public void emptyPositionRule() {
