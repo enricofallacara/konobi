f46c61a62f045863e7b4f45075de34aa408335c2
pindri <patrick.indri@outlook.it>
2020-01-28 10:19:01 +0100

    searchForEndingPoints tested

--- src/main/java/core/EndGameRule.java
+++ src/main/java/core/EndGameRule.java
@@ -2,30 +2,26 @@ package core;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
 public class EndGameRule {
 
-    private static HashSet<Cell> table;
+    private static HashSet<Cell> table = new HashSet<>();
 
     public static boolean query(Board board, Player player) {
-        // TODO: NON SERVE UNA FUNZIONE PER OTTENERE I VICINI "POSITIVI", PER
-        // DUE MOTIVI: 1) NON E DETTO CHE UNA CATENA SI SVILUPPI SOLAMENTE
-        // "IN AVANTI"; 2) SI PUO USARE UNA STRUTTURA DATI (COME UNA HASHTABLE)
-        // PER RICORDARE CHI E STATO ATTRAVERSATO E CHI NO.
-        ArrayList<Cell> endpoints = getEndPoints(board, player);
-        table = new HashSet<>();
-        if (endpoints.size() == 0) {
+        ArrayList<Cell> startingPoints = getStartingPoints(board, player);
+
+        if (startingPoints.size() == 0) {
             return false;
         }
-        for (Cell startpoint : endpoints) {
-        }
-        return false;
+
+        table.clear();
+
+        return startingPoints.stream().anyMatch(x -> searchForEndingEdge(x, board, player));
     }
 
-    public static ArrayList<Cell> getEndPoints(Board board, Player player) {
+    public static ArrayList<Cell> getStartingPoints(Board board, Player player) {
         Color color = player.getColor();
         int size = board.getSize();
         int[] startIdxs = (color == Color.white) ? new int[]{0, size, 0, 1} : new int[]{0, 1, 0, size};
@@ -34,8 +30,15 @@ public class EndGameRule {
     }
 
 
-    private boolean propagate(Cell current, Player player) {
+    public static boolean searchForEndingEdge(Cell current, Board board, Player player) {
+        System.out.println(current.getCoordinates().x + " " + current.getCoordinates().y);
+        if (board.isOnEndingEdge(current.getCoordinates(), player)) { return true; }
 
+        table.add(current);
+
+        for (Cell neighbour : board.getColoredNeighbours(current.getCoordinates(), 1, player, (x, y) -> true)) {
+           if (!table.contains(neighbour)) { return searchForEndingEdge(neighbour, board, player); }
+        }
 
         return false;
     }
--- src/test/java/core/endGameTest.java
+++ src/test/java/core/endGameTest.java
@@ -5,8 +5,7 @@ import org.junit.Test;
 import java.awt.*;
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class endGameTest {
 
@@ -14,11 +13,34 @@ public class endGameTest {
     public void testGetEndpoints() {
         Board board = new Board(11);
         Player player = new Player(Color.white);
-        assertTrue(EndGameRule.getEndPoints(board, player).isEmpty());
+        assertTrue(EndGameRule.getStartingPoints(board, player).isEmpty());
         board.setCell(new Point(3, 0), Color.white);
         board.setCell(new Point(4, 0), Color.black);
         ArrayList<Cell> expected = new ArrayList<>();
         expected.add(board.getCell(new Point(3, 0)));
-        assertEquals(expected, EndGameRule.getEndPoints(board, player));
+        assertEquals(expected, EndGameRule.getStartingPoints(board, player));
+    }
+
+    @Test
+    public void testSearchForEndingEdge() {
+        Board board = new Board(4);
+        Player player = new Player(Color.black);
+        Point point = new Point(1, 0);
+
+        board.setCell(new Point(1, 0), Color.black);
+        board.setCell(new Point(2, 1), Color.black);
+        board.setCell(new Point(2, 2), Color.black);
+        board.setCell(new Point(2, 3), Color.black);
+        board.setCell(new Point(3, 3), Color.black);
+
+        board.setCell(new Point(2, 0), Color.white);
+        board.setCell(new Point(1, 1), Color.white);
+        board.setCell(new Point(1, 2), Color.white);
+        board.setCell(new Point(1, 3), Color.white);
+
+        assertTrue(EndGameRule.searchForEndingEdge(board.getCell(point), board, player));
+
+        board.setCell(new Point(2, 2), Color.white);
+        assertFalse(EndGameRule.searchForEndingEdge(board.getCell(point), board, player));
     }
 }
