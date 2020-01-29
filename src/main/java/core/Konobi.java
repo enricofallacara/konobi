45c5465e75e804a51ff01a321c55b19eba60699d
federicopigozzi <pigozzife@gmail.com>
2020-01-29 12:09:40 +0100

    need for test push

--- src/main/java/core/Konobi.java
+++ src/main/java/core/Konobi.java
@@ -24,6 +24,6 @@ public class Konobi {
                 supervisor.newMove(newPoint);
             }
         }
-        userInterface.endGame(supervisor.getCurrentPlayer());
+        //userInterface.endGame(supervisor.getCurrentPlayer());
     }
 }
