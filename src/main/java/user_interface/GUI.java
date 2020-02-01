df71ea0b6484915ea5dc1348c1b26bf2a658cfc4
enricofallacara <enrico.fallacara@gmail.com>
2020-02-01 17:03:41 +0100

    implemented a better chessboard in JavaFX

--- src/main/java/user_interface/GUI.java
+++ src/main/java/user_interface/GUI.java
@@ -1,9 +1,13 @@
 package user_interface;
 
+import core.Board;
+import core.Player;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -12,14 +16,16 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
-public class GUI extends Application {
+public class GUI extends Application implements UserInterface{
     private final int SIZE = 50;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+   // public static void main(String[] args) {
+  //      launch(args);
+   // }
 
     @Override
     public void start(Stage primaryStage) {
@@ -27,9 +33,8 @@ public class GUI extends Application {
     }
 
     private void initUI(Stage stage) {
-        StackPane root = new StackPane();
 
-        Pane board = new Pane();
+        GridPane board = new GridPane();
 
         for (int row = 0; row < 11; row++) {
             for (int col = 0; col < 11; col++) {
@@ -37,23 +42,72 @@ public class GUI extends Application {
                 Rectangle r = new Rectangle(col * SIZE, row * SIZE,
                         SIZE, SIZE);
 
+
                 if ((col + row) % 2 == 0) {
                     r.setFill(Color.BLUE);
                 } else {
                     r.setFill(Color.BEIGE);
                 }
 
-                board.getChildren().add(r);
+                board.addRow(row,r);
             }
 
         }
-        root.getChildren().add(board);
 
-        Scene scene = new Scene(root, Color.WHITESMOKE);
+        board.setOnMouseClicked(e -> {Rectangle rect = new Rectangle(e.getX() * SIZE, e.getY() * SIZE, SIZE, SIZE);
+            rect.setFill(Color.BLACK);
+            board.add(rect, (int)e.getX()/SIZE, (int)e.getY()/SIZE);
+        });
 
+        Scene scene = new Scene(board, Color.WHITESMOKE);
         stage.setTitle("ChessBoard");
         stage.setScene(scene);
         stage.show();
+
+
+
+
+
+    }
+
+    @Override
+    public Point getInput(Player player) {
+        return null;
+    }
+
+    @Override
+    public boolean askPieRule() {
+        return false;
+    }
+
+    @Override
+    public void notifyEndGame(Player player) {
+
+    }
+
+    @Override
+    public int askSize() {
+        return 0;
+    }
+
+    @Override
+    public void notifyPass() {
+
+    }
+
+    @Override
+    public void display(Board board) {
+
+    }
+
+    @Override
+    public void notifyInvalidMove() {
+
+    }
+
+    @Override
+    public void initialize() {
+
     }
 }
 
