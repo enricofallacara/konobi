ecf78a5021fd5bd34e7fbcfd45ea6934ca0f76c2
federicopigozzi <pigozzife@gmail.com>
2020-02-01 17:33:11 +0100

    implemented initialize in GUI and simplified loops

--- src/main/java/user_interface/GUI.java
+++ src/main/java/user_interface/GUI.java
@@ -3,55 +3,45 @@ package user_interface;
 import core.Board;
 import core.Player;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.awt.Point;
 import java.io.IOException;
+import java.util.Optional;
 
 public class GUI extends Application implements UserInterface{
     private final int SIZE = 50;
-
-
-   // public static void main(String[] args) {
-  //      launch(args);
-   // }
+    private Stage stage;
 
     @Override
     public void start(Stage primaryStage) {
-        initUI(primaryStage);
+        stage = primaryStage;
+        initUI();
     }
 
-    private void initUI(Stage stage) {
+    public void initUI() {
 
         GridPane board = new GridPane();
 
         for (int row = 0; row < 11; row++) {
             for (int col = 0; col < 11; col++) {
-
-                Rectangle r = new Rectangle(col * SIZE, row * SIZE,
-                        SIZE, SIZE);
-
-
-                if ((col + row) % 2 == 0) {
-                    r.setFill(Color.BLUE);
-                } else {
-                    r.setFill(Color.BEIGE);
-                }
-
+                Rectangle r = new Rectangle(col * SIZE,row * SIZE , SIZE, SIZE);
+                r.setFill( (col + row) % 2 == 0 ? Color.BEIGE : Color.BLUE);
                 board.addRow(row,r);
             }
-
         }
 
         board.setOnMouseClicked(e -> {Rectangle rect = new Rectangle(e.getX() * SIZE, e.getY() * SIZE, SIZE, SIZE);
@@ -107,7 +97,22 @@ public class GUI extends Application implements UserInterface{
 
     @Override
     public void initialize() {
+        HBox initPane = new HBox();
+        Button startButton = new Button("Start");
+        startButton.setOnAction((ActionEvent e) -> System.out.println("start"));
+        Button endButton = new Button("Quit");
+        endButton.setOnAction((ActionEvent e) -> Platform.exit());
+        Button rulesButton = new Button("Rules");
+        rulesButton.setOnAction((ActionEvent e) -> System.out.println("RULES NOT AVAILABLE YET"));
+        initPane.setPadding(new Insets(25, 25, 25, 25));
+        initPane.getChildren().add(startButton);
+        initPane.getChildren().add(endButton);
+        initPane.getChildren().add(rulesButton);
+        Scene scene = new Scene(initPane);
 
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
