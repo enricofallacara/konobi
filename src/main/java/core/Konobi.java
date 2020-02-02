5976d4dcc01aaeefca08263fd3af88da1d7031e0
pindri <patrick.indri@outlook.it>
2020-02-02 13:33:36 +0100

    Refactored game flow, Main calls Konobi class

--- src/main/java/core/Konobi.java
+++ src/main/java/core/Konobi.java
@@ -17,42 +17,25 @@ public class Konobi {
     public void play() {
         while(!supervisor.isEndGame()) {
             playTurn();
-
         }
 
+        userInterface.display(supervisor.getBoard());
+        Player winner = supervisor.getLastPlayer();
+        userInterface.notifyEndGame(winner);
     }
 
     private void playTurn() {
-        System.out.println("Turn");
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    /*
-    public static void main(String[] args) {
-        UserInterface userInterface = new Console();
-        Supervisor supervisor = new Supervisor(userInterface.initialize());
-
-        while (!supervisor.isEndGame()) {
-            userInterface.display(supervisor.getBoard());
-            if (supervisor.isPassRule()) {
-                userInterface.notifyPass();
-                continue;
-            }
-            if (supervisor.isPieRule() && userInterface.askPieRule()) {
-                supervisor.performPieRule();
-                continue;
-            }
-            while (!supervisor.newMove(userInterface.getInput(supervisor.getCurrentPlayer()))) {
-                userInterface.notifyInvalidMove();
-            }
-        }
         userInterface.display(supervisor.getBoard());
-        userInterface.notifyEndGame(supervisor.getLastPlayer());
+        if (supervisor.isPassRule()) {
+            userInterface.notifyPass();
+            return;
+        }
+        if (supervisor.isPieRule() && userInterface.askPieRule()) {
+            supervisor.performPieRule();
+            return;
+        }
+        while (!supervisor.newMove(userInterface.getInput(supervisor.getCurrentPlayer()))) {
+            userInterface.notifyInvalidMove();
+        }
     }
-    */
 }
