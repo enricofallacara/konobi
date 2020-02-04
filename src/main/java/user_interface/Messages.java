ddfc1d37929d4d3b0174832991abf1f38a72975c
pindri <patrick.indri@outlook.it>
2020-02-04 17:36:33 +0100

    Messages are now final

--- src/main/java/user_interface/Messages.java
+++ src/main/java/user_interface/Messages.java
@@ -1,14 +1,14 @@
 package user_interface;
 
 public class Messages {
-    public static String invalidMove = "The Selected Position is Invalid!";
-    public static String endGame = "Congratulations %s (color %s), you won!";
-    public static String playerTurn = "%s's turn! %s";
-    public static String pass = "YOU SHALL PASS";
-    public static String pieRule = "PlayerTwo, do you want to apply the Pie Rule? (y/n)";
-    public static String xInput = "Please insert next X coordinate:";
-    public static String yInput = "Please insert next Y coordinate:";
-    public static String consoleLogo =  " _   __                  _     _ \n" +
+    public final static String invalidMove = "The Selected Position is Invalid!";
+    public final static String endGame = "Congratulations %s (color %s), you won!";
+    public final static String playerTurn = "%s's turn! %s";
+    public final static String pass = "YOU SHALL PASS";
+    public final static String pieRule = "PlayerTwo, do you want to apply the Pie Rule? (y/n)";
+    public final static String xInput = "Please insert next X coordinate:";
+    public final static String yInput = "Please insert next Y coordinate:";
+    public final static String consoleLogo =  " _   __                  _     _ \n" +
                                         "| | / /                 | |   (_)\n" +
                                         "| |/ /  ___  _ __   ___ | |__  _ \n" +
                                         "|    \\ / _ \\| '_ \\ / _ \\| '_ \\| |\n" +
