eafad78214f91d1c75b68416289d7a8121f53451
federicopigozzi <pigozzife@gmail.com>
2020-02-08 15:33:24 +0100

    refactor EmptyRule tests

---
+++ src/test/java/core/testEmptyPositionRule.java
@@ -0,0 +1,27 @@
+package core;
+
+import core.Entities.Board;
+import core.Entities.Color;
+import core.Rules.EmptyRule;
+import org.junit.Test;
+import java.awt.Point;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class testEmptyPositionRule {
+    private Board board = new Board(11);
+    private Point point = new Point(1, 2);
+
+    @Test
+    public void emptyPositionRuleValid() {
+        assertTrue(EmptyRule.isValid(point, board));
+    }
+
+    @Test
+    public void emptyPositionRuleInvalid() {
+        board.setCell(new Point(1, 2), Color.black);
+        assertFalse(EmptyRule.isValid(point, board));
+    }
+
+}
