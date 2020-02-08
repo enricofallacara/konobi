d90f6e761eae2e44599d1f377bffee81d32d63fa
federicopigozzi <pigozzife@gmail.com>
2020-02-08 18:41:41 +0100

    added TODO and minor fixes

--- src/main/java/UI/Console/ConsoleInputHandler.java
+++ src/main/java/UI/Console/ConsoleInputHandler.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
 public class ConsoleInputHandler {
 
     private final Scanner scanner;
@@ -16,48 +15,46 @@ public class ConsoleInputHandler {
     public boolean askPieRule() {
         ConsoleMessageWriter.pieRuleAskMessage();
         String answer;
-        try{
+        try {
             answer = scanner.next();
             if(!(answer.equals("y") || answer.equals("n")))
                 throw new InputPieRuleException(Messages.invalidStringInput);
-        }catch(InputPieRuleException e){
+        } catch(InputPieRuleException e){
             System.out.println(e.getMessage());
             scanner.next();
             return askPieRule();
         }
         return answer.toLowerCase().equals("y");
     }
-
-    public int getInteger(){
-
-        try{
-            if(scanner.hasNextInt())
+    // TODO: controllare anche che gli interi passati siano nel range giusto, magari
+    //  con relativa eccezione
+    public int getInteger() {
+        try {
+            if (scanner.hasNextInt())
                 return scanner.nextInt();
             else
                 throw new InputMismatchException();
             }
-            catch(InputMismatchException e){
+            catch (InputMismatchException e){
                 System.out.println(Messages.invalidIntegerInput);
                 scanner.next();
                 return getInteger();
             }
-
     }
 
     public int askSize() {
         System.out.println(Messages.askSize);
         int size;
-        try{
+        try {
             size = getInteger();
             if(size < 3 || size > 11)
                 throw new InputSizeException(Messages.invalidIntegerInput);
 
-        }catch(InputSizeException e){
+        } catch(InputSizeException e){
             System.out.println(e.getMessage());
             return askSize();
         }
         return size;
-
     }
 
     public Point getInput(Player player) {
--- src/main/java/core/Rules/EndGameRule.java
+++ src/main/java/core/Rules/EndGameRule.java
@@ -6,9 +6,9 @@ import java.util.HashSet;
 import java.util.stream.Stream;
 
 public class EndGameRule implements Rule{
-    private HashSet<Cell> table;
+    private HashSet<Cell> visitedTable;
 
-    public EndGameRule(){ table = new HashSet<>(); }
+    public EndGameRule(){ visitedTable = new HashSet<>(); }
 
     @Override
     public boolean isValid(Supervisor supervisor) {
@@ -23,14 +23,14 @@ public class EndGameRule implements Rule{
         int size = board.getSize();
         int[] startIdxs = (color == Color.white) ? new int[]{0, size, 0, 1} :  new int[]{0, 1, 0, size};
         return Arrays.stream(board.slice(startIdxs[0], startIdxs[1], startIdxs[2], startIdxs[3])).
-                filter(x -> x.hasThisColor(color));//.collect(Collectors.toCollection(ArrayList::new));
+                filter(x -> x.hasThisColor(color));
     }
 
     public boolean searchForEndingEdge(Cell current, Board board, Color color) {
         if (board.isOnEndingEdge(current.getCoordinates(), color)) { return true; }
-        table.add(current);
+        visitedTable.add(current);
         for (Cell neighbour : board.getColoredNeighbours(current.getCoordinates(), 1, color, (x, y) -> true).toArray(Cell[]::new)) {
-            if (!table.contains(neighbour)) { return searchForEndingEdge(neighbour, board, color); }
+            if (!visitedTable.contains(neighbour)) { return searchForEndingEdge(neighbour, board, color); }
         }
         return false;
     }
