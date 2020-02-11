9f864b61656d1e2b5709bfe3d10411ee023b49f7
federicopigozzi <pigozzife@gmail.com>
2020-02-11 14:00:21 +0100

    more Neighbourhood test

--- src/test/java/Core/Entities/neighbourhoodTest.java
+++ src/test/java/Core/Entities/neighbourhoodTest.java
@@ -2,20 +2,21 @@ package Core.Entities;
 
 import org.junit.Test;
 
-import java.awt.*;
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class neighbourhoodTest {
 
     private final Board board = new Board(11);
+    private final Point p = new Point(3, 3);
 
     @Test
     public void testMooreNeighbours() {
-        Point p = new Point(3,3);
         ArrayList<Cell> mooreNeighbours = Neighbourhood.getMooreNeighbours(board, p, 1).collect(Collectors.toCollection(ArrayList::new));
         ArrayList<Cell> expectedNeighbours = new ArrayList<>() {{
             add(board.getCell(p));
@@ -32,7 +33,6 @@ public class neighbourhoodTest {
 
     @Test
     public void testStrongNeighbours(){
-        Point p = new Point(3,3);
         ArrayList<Cell> strongNeighbours = Neighbourhood.getNeighbours(board, p, 1, Neighbourhood::isStrongNeighbour).collect(Collectors.toCollection(ArrayList::new));
         ArrayList<Cell> expectedNeighbours = new ArrayList<>() {{
             add(board.getCell(new Point(p.x,p.y + 1)));
@@ -44,7 +44,6 @@ public class neighbourhoodTest {
 
     @Test
     public void testWeakNeighbours(){
-        Point p = new Point(3,3);
         ArrayList<Cell> weakNeighbours = Neighbourhood.getNeighbours(board, p, 1, Neighbourhood::isWeakNeighbour).collect(Collectors.toCollection(ArrayList::new));
         ArrayList<Cell> expectedNeighbours = new ArrayList<>() {{
             add(board.getCell(new Point(p.x+1,p.y+1)));
@@ -53,4 +52,22 @@ public class neighbourhoodTest {
             add(board.getCell(new Point(p.x-1,p.y-1))); }};
         assertEquals(new HashSet<>(weakNeighbours), new HashSet<>(expectedNeighbours));
     }
+
+    @Test
+    public void testColoredNeighboursNone() {
+        ArrayList<Cell> coloredNeighbours = Neighbourhood.getColoredNeighbours(board, p, 1, Color.black, Neighbourhood::isWeakNeighbour).collect(Collectors.toCollection(ArrayList::new));
+        assertTrue(coloredNeighbours.isEmpty());
+    }
+
+    @Test
+    public void testColoredNeighboursSome() {
+        board.setCell(new Point(p.x + 1, p.y), Color.black);
+        board.setCell(new Point(p.x + 1, p.y + 1), Color.white);
+        board.setCell(new Point(p.x - 1, p.y), Color.white);
+        board.setCell(new Point(p.x + 1, p.y - 1), Color.black);
+        ArrayList<Cell> coloredNeighbours = Neighbourhood.getColoredNeighbours(board, p, 1, Color.black, Neighbourhood::isWeakNeighbour).collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<Cell> expectedNeighbours = new ArrayList<>() {{ add(board.getCell(new Point(p.x+1,p.y-1))); }};
+        assertEquals(expectedNeighbours, coloredNeighbours);
+    }
+
 }
