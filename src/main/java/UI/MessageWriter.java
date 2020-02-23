7ceee5ee90f49f5d900e8daac4e4c7633b778df7
pindri <patrick.indri@outlook.it>
2020-02-23 16:42:49 +0100

    added missing file

---
+++ src/main/java/UI/MessageWriter.java
@@ -0,0 +1,14 @@
+package UI;
+
+import core.Entities.Player;
+
+public interface MessageWriter {
+
+    void notifyPass();
+
+    void notifyEndGame(Player player);
+
+    void notifyInvalidMove();
+
+    void notifyPieRule();
+}
