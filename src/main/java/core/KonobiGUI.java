2876c3641df4655e2f4ff9921971c02df8b5d8ee
enricofallacara <enrico.fallacara@gmail.com>
2020-02-24 16:18:50 +0100

    added a TODO

--- src/main/java/core/KonobiGUI.java
+++ src/main/java/core/KonobiGUI.java
@@ -2,6 +2,7 @@ package core;
 
 import UI.Console.ConsoleBoardWriter;
 import UI.Console.ConsoleInputHandler;
+import UI.GUI.GUI;
 import UI.GUI.GUIInputHandler;
 import UI.GUI.GUIMessageWriter;
 import UI.InputHandler;
@@ -12,6 +13,8 @@ import core.Entities.StatusSupervisor;
 import core.Rules.EndGameRule;
 import core.Rules.PassRule;
 import core.Rules.PieRule;
+import javafx.application.Application;
+
 import java.awt.Point;
 
 
@@ -21,6 +24,8 @@ public class KonobiGUI extends AbstractKonobi<GUIMessageWriter, GUIInputHandler>
     public KonobiGUI(int size, GUIMessageWriter mw, GUIInputHandler ih) {
         super(size, mw, ih);
     }
+
+    //TODO: Vedere se implementare metodo play come accade in KonobiConsole
 
     public Player getCurrentPlayer() { return supervisor.getCurrentPlayer(); }
 
